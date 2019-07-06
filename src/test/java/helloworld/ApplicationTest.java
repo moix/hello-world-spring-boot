e96b19b561a13d1abbb3c9a04545feb6ccf38e17
Moises Cruz <mocruz@theworkshop.com>
2019-07-06 10:45:40 +0200

    initial commit hello world project sb

---
+++ src/test/java/helloworld/ApplicationTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helloworld;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = PoliteController.class)
+public class ApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void homePage() throws Exception {
+        // N.B. jsoup can be useful for asserting HTML content
+        mockMvc.perform(get("/index.html"))
+                .andExpect(content().string(containsString("Get your greeting")));
+    }
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+    @Test
+    public void greetingWithUser() throws Exception {
+        mockMvc.perform(get("/hello").param("name", "Alex"))
+                .andExpect(content().string(containsString("Hello, Alex!")));
+    }
+
+    @Test
+    public void bye() throws Exception {
+        mockMvc.perform(get("/bye"))
+                .andExpect(content().string(containsString("Bye, World!")));
+    }
+
+    @Test
+    public void byeWithUser() throws Exception {
+        mockMvc.perform(get("/bye").param("name", "Sofia"))
+                .andExpect(content().string(containsString("Bye, Sofia!")));
+    }
+
+}
